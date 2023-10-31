2c0528c821de2395a1240484b28a1d922abac3d2
Maxmerock123 <Maxmerock@gmail.com>
2023-10-31 08:18:49 +0700

    add more choices, change expense and income input
    change to radio type but still had to get a value from radio
    button to react components. need to find a way to do "onChange"
    that work on radio button.

--- src/components/Add.js
+++ src/components/Add.js
@@ -5,33 +5,39 @@ const Add = ({onClick}) => {
   const [name,setName] = useState("")
   const [expense,setExpense] = useState("")
   const [income,setIncome] = useState("")
+  const [type,setType] = useState("")
   const [catagory,setCatagory] = useState("")
   const [date,setDate] = useState("")
+
 
   return (
     <>
     <form>
       <div>
         <label for="name">Transaction Name </label>
-        <input type="text" id="name" name="name" onChange={(e)=>setName(e.target.value)}/>
+        <input type="text" id="name" name="name" autocomplete="off" onChange={(e)=>setName(e.target.value)}/>
       </div>  
       <div>
-        <label for="expense">expense </label>
-        <input type="number" id="expense" name="expense" onChange={(e)=>setExpense(e.target.value)}/>
+        <label for="expense">Amount $ </label>
+        <input type="number" id="amount" name="amount" autocomplete="off" onChange={(e)=>setExpense(e.target.value)}/>
       </div>  
       <div>
-        <label for="income">Amount </label>
-        <input type="number" id="income" name="income" onChange={(e)=>setIncome(e.target.value)}/>
+        <input type="radio" id="expense" name="transaction-type" onChange={(e)=>setType(e.target.value)}></input>
+        <label for="expense" id="container-expense">Expense </label>
+        <input type="radio" id="income" name="transaction-type" onChange={(e)=>setType(e.target.value)}></input>
+        <label for="income" id="container-income">Income </label>
       </div>  
       <div>
         <lable for="catagory-form">Catagory </lable>
         <select name="catagory" form="catagory-form" onChange={(e)=>setCatagory(e.target.value)}>
           <option value="none">-Select a Catagory-</option>
-          <option value="food-beverage">food-beverage</option>
-          <option value="transport">transport</option>
-          <option value="bill">bill</option>
-          <option value="shopping">shopping</option>
-          <option value="others">others</option>
+          <option value="food-beverage">Food-beverage</option>
+          <option value="transport">Transport</option>
+          <option value="bill">Bill</option>
+          <option value="shopping">Shopping</option>
+          <option value="salary">Salary</option>
+          <option value="gift">Gift</option>
+          <option value="others">Others</option>
         </select>
       </div>  
       <div>
