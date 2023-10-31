054469ce904ee2a6fcd910e9d1d75ad8e936a575
Maxmerock123 <Maxmerock@gmail.com>
2023-10-31 07:52:01 +0700

    add onChange function to every form element
    add "onChange={(e)=>setDate(e.target.value)}"

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const Add = ({onClick}) => {
   const [name,setName] = useState("")
-  const [expense,setExpese] = useState("")
+  const [expense,setExpense] = useState("")
   const [income,setIncome] = useState("")
   const [catagory,setCatagory] = useState("")
   const [date,setDate] = useState("")
@@ -13,19 +13,20 @@ const Add = ({onClick}) => {
     <form>
       <div>
         <label for="name">Transaction Name </label>
-        <input type="text" id="name" name="name"/>
+        <input type="text" id="name" name="name" onChange={(e)=>setName(e.target.value)}/>
       </div>  
       <div>
         <label for="expense">expense </label>
-        <input type="number" id="expense" name="expense"/>
+        <input type="number" id="expense" name="expense" onChange={(e)=>setExpense(e.target.value)}/>
       </div>  
       <div>
         <label for="income">Amount </label>
-        <input type="number" id="income" name="income"/>
+        <input type="number" id="income" name="income" onChange={(e)=>setIncome(e.target.value)}/>
       </div>  
       <div>
         <lable for="catagory-form">Catagory </lable>
-        <select name="catagory" form="catagory-form">
+        <select name="catagory" form="catagory-form" onChange={(e)=>setCatagory(e.target.value)}>
+          <option value="none">-Select a Catagory-</option>
           <option value="food-beverage">food-beverage</option>
           <option value="transport">transport</option>
           <option value="bill">bill</option>
@@ -34,8 +35,8 @@ const Add = ({onClick}) => {
         </select>
       </div>  
       <div>
-        <label for="date">Date </label>
-        <input type="date" id="date" name="date"/>
+        <label for="date" >Date </label>
+        <input type="date" id="date" name="date" onChange={(e)=>setDate(e.target.value)}/>
       </div>
     </form>
     <button type="submit">Submit</button>
